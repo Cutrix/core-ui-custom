c8454e4f67eef669410b438f26721a9f290cf67d
cutr21 <houessinonlandry@gmail.com>
2020-09-24 00:34:46 +0000

    init

---
+++ src/views/tables/tableData.js
@@ -0,0 +1,7 @@
+const tablesData = [
+    { API: 'api livre', TECHNOLOGIE: "Symfony", DEVELOPER: "Bamba Mohammed" },
+    { API: 'api audio', TECHNOLOGIE: "Symfony", DEVELOPER: "Bamba Mohammed" },
+    { API: 'api BD', TECHNOLOGIE: "Symfony", DEVELOPER: "Bamba Mohammed" },
+];
+
+export default tablesData
